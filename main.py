2c9670a9f6342bce563cd3406beb35f6f78d04fd
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-07-21 13:45:57 -0300

    First file with basic code

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import pikepdf
+
+""" Structure directories
+"""
+SOURCE_PDF_DIR = 'source_PDF'
+TARGET_PDF_DIR = 'target_PDF'
+SOURCE_PDF_LIST = os.listdir(SOURCE_PDF_DIR)
+
+if not SOURCE_PDF_LIST:
+    print('No se encuentra la carpeta especificada')
+else:
+    for pdf_file in SOURCE_PDF_LIST:
+        with pikepdf.open(
+            f'./{SOURCE_PDF_DIR}/{pdf_file}'
+        )
+        as pdf_source_file:
+            actually_version = pdf_source_file.pdf_version
+            pdf_source_file.save(
+                f'./{TARGET_PDF_DIR}/{pdf_file}',
+                force_version='1.4'
+            )
