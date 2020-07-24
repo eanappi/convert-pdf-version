8f6ead44328d81ebced2f9762ab1d9253f6c5402
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-07-24 17:47:40 -0300

    FIX - Update init code

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import pikepdf
 
 """ Structure directories
 """
-SOURCE_PDF_DIR = 'source_PDF'
-TARGET_PDF_DIR = 'target_PDF'
-SOURCE_PDF_LIST = os.listdir(SOURCE_PDF_DIR)
 
-if not SOURCE_PDF_LIST:
-    print('No se encuentra la carpeta especificada')
-else:
-    for pdf_file in SOURCE_PDF_LIST:
-        with pikepdf.open(f'./{SOURCE_PDF_DIR}/{pdf_file}') as pdf_source_file:
-            actually_version = pdf_source_file.pdf_version
-            pdf_source_file.save(f'./{TARGET_PDF_DIR}/{pdf_file}', force_version='1.4')
+SOURCE_PDF_DIR = input("Input source folder: ")
+TARGET_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
+SOURCE_PDF_LIST = os.listdir(path=f'{SOURCE_PDF_DIR}')
+
+for file in SOURCE_PDF_LIST:
+    if not SOURCE_PDF_LIST:
+        print('No se encuentra la carpeta especificada')
+    else:
+        for pdf_file in SOURCE_PDF_LIST:
+            if file.endswith('.pdf'):
+                with pikepdf.open(
+                    f'{SOURCE_PDF_DIR}/{pdf_file}') as pdf_source_file:
+                    pdf_source_file.save(
+                        f'{TARGET_PDF_DIR}/{pdf_file}', force_version='1.4')
