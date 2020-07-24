ee21506830ba5fa08dff616e6714bf1a6e325ef6
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-07-24 17:31:14 -0300

    Add enviroment and basic structure

--- main.py
+++ main.py
@@ -11,12 +11,6 @@ if not SOURCE_PDF_LIST:
     print('No se encuentra la carpeta especificada')
 else:
     for pdf_file in SOURCE_PDF_LIST:
-        with pikepdf.open(
-            f'./{SOURCE_PDF_DIR}/{pdf_file}'
-        )
-        as pdf_source_file:
+        with pikepdf.open(f'./{SOURCE_PDF_DIR}/{pdf_file}') as pdf_source_file:
             actually_version = pdf_source_file.pdf_version
-            pdf_source_file.save(
-                f'./{TARGET_PDF_DIR}/{pdf_file}',
-                force_version='1.4'
-            )
+            pdf_source_file.save(f'./{TARGET_PDF_DIR}/{pdf_file}', force_version='1.4')
