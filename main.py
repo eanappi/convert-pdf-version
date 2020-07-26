9cb67af02747223fb27de7ea91effef31e2215db
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-07-26 18:00:32 -0300

    Validation functions and import pikepdf module

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ import pikepdf
 """
 
 SOURCE_PDF_DIR = input("Input source folder: ")
-TARGET_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
-SOURCE_PDF_LIST = os.listdir(path=f'{SOURCE_PDF_DIR}')
 
+if not os.path.exists(SOURCE_PDF_DIR):
+    print('Folder isn\'t found.')
+    exit()
+
+SOURCE_PDF_LIST = os.listdir(path=f'{SOURCE_PDF_DIR}')
+TARGET_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
+
+if not SOURCE_PDF_LIST:
+    print('Folder is empty')
+    exit()
+
+"""
 for file in SOURCE_PDF_LIST:
     if not SOURCE_PDF_LIST:
         print('No se encuentra la carpeta especificada')
@@ -18,3 +28,4 @@ for file in SOURCE_PDF_LIST:
                     f'{SOURCE_PDF_DIR}/{pdf_file}') as pdf_source_file:
                     pdf_source_file.save(
                         f'{TARGET_PDF_DIR}/{pdf_file}', force_version='1.4')
+"""
