425ad11b5e7d3fc01017e463926ffd29cbe8c146
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-07-28 17:02:48 -0300

    Refactory - all to functions

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import os
 import pikepdf
 
-""" Structure directories
-"""
-
 SOURCE_PDF_DIR = input('Input source folder: ')
-TARGET_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
+DESTINY_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
 
-with os.scandir(path=f'{SOURCE_PDF_DIR}') as files:
-    for file in files:
-        if file.is_file(follow_symlinks=False) and file.name.endswith('.pdf'):
-            print(file.name)
- 
-    
-"""
-for file in SOURCE_PDF_LIST:
-    if not SOURCE_PDF_LIST:
-        print('No se encuentra la carpeta especificada')
-    else:
-        for pdf_file in SOURCE_PDF_LIST:
-            if file.endswith('.pdf'):
-                with pikepdf.open(
-                    f'{SOURCE_PDF_DIR}/{pdf_file}') as pdf_source_file:
-                    pdf_source_file.save(
-                        f'{TARGET_PDF_DIR}/{pdf_file}', force_version='1.4')
-"""
+
+def convert_PDF_version(path_src, path_des):
+    with os.scandir(path=f'{path_src}') as files:
+        for file in files:
+            is_file = file.is_file(follow_symlinks=False)
+            is_pdf = file.name.endswith('.pdf')
+
+            if is_file and is_pdf:
+                file_src = (os.path.join(path_src, file.name))
+                file_des = (os.path.join(path_des, file.name))
+                
+                with pikepdf.open(file_src) as pdf_source_file:
+                    pdf_source_file.save(file_des, force_version='1.4')
+                    
+
+                    
+if not os.path.exists(SOURCE_PDF_DIR):
+    exit('Path isn\'t found.')
+elif not os.path.exists(DESTINY_PDF_DIR):
+    os.mkdir(DESTINY_PDF_DIR)
+
+convert_PDF_version(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
