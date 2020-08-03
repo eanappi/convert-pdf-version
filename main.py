966702c94a2db93248c2398f4290ea0432fc1c31
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-08-03 17:32:08 -0300

    Add validation of the result convertion

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import os
-import sys
-import fnmatch
+import os, sys, fnmatch
 import pikepdf
 
 SOURCE_PDF_DIR = input('Input source folder: ')
 DESTINY_PDF_DIR = f'{SOURCE_PDF_DIR}/convert_PDF'
+DESTINY_PDF_VER = '1.5'
 
 def convert_PDF_version(path_src, path_des):
     with os.scandir(path=f'{path_src}') as files:
@@ -17,13 +16,16 @@ def convert_PDF_version(path_src, path_des):
                 file_des = (os.path.join(path_des, file.name))
 
                 with pikepdf.open(file_src) as pdf_source_file:
-                    pdf_source_file.save(file_des, force_version='1.4')
+                    pdf_source_file.save(
+                        file_des, force_version=DESTINY_PDF_VER)
                     sys.stdout.write('.')
 
 def compare_files(path_src, path_dst):
-    count_src = fnmatch.filter(os.listdir(path_src), '*.pdf')
-    count_dst = fnmatch.filter(os.listdir(path_dst), '*.pdf')
-    
+    list_src = fnmatch.filter(os.listdir(path_src), '*.pdf')
+    list_dst = fnmatch.filter(os.listdir(path_dst), '*.pdf')
+    list_difference = list(set(list_src)-set(list_dst))
+
+    print('\nThe different files are: ', list_difference)
 
 
 if not os.path.exists(SOURCE_PDF_DIR):
@@ -31,5 +33,5 @@ if not os.path.exists(SOURCE_PDF_DIR):
 elif not os.path.exists(DESTINY_PDF_DIR):
     os.mkdir(DESTINY_PDF_DIR)
 
-#convert_PDF_version(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
-compare_files()
+convert_PDF_version(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
+compare_files(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
