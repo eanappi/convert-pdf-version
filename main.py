dd26acbf4278fee1fe783dec7ef9ab0c776a821c
Ernesto Alejandro Nappi <eanappi@gmail.com>
2020-08-03 14:25:53 -0300

    IN_PROGRESS - Verify files completed

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ def convert_PDF_version(path_src, path_des):
                     pdf_source_file.save(file_des, force_version='1.4')
                     sys.stdout.write('.')
 
-#def compare_files(path_src, path_dest):
+def compare_files(path_src, path_dst):
+    count_src = fnmatch.filter(os.listdir(path_src), '*.pdf')
+    count_dst = fnmatch.filter(os.listdir(path_dst), '*.pdf')
+    
 
 
 if not os.path.exists(SOURCE_PDF_DIR):
@@ -28,4 +31,5 @@ if not os.path.exists(SOURCE_PDF_DIR):
 elif not os.path.exists(DESTINY_PDF_DIR):
     os.mkdir(DESTINY_PDF_DIR)
 
-convert_PDF_version(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
+#convert_PDF_version(SOURCE_PDF_DIR, DESTINY_PDF_DIR)
+compare_files()
